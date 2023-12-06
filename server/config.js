5c53daffb4b62d78ffba0f4387fa57a7eb64dd64
Harsh <harshb15003@gmail.com>
2023-12-07 04:25:26 +0530

    razorpay integeration

---
+++ server/config.js
@@ -0,0 +1,8 @@
+export default {
+    JWT_SECRET:"TqiQ9eam2FMcmd4Nafic5wEjBktVXObuMuDHrJc1elY=",
+    ATLAS_URI:"mongodb+srv://harshb15003:harsh1510@cluster0.gr6iatu.mongodb.net/FutureFinance",
+    RAZORPAY_PUBLIC_ID: "rzp_test_rrpFDSyVYUuEE4",
+    RAZORPAY_SECRET_ID:"f61Os4eHrG2Bk2IIQzqd9TqV"
+
+
+}
