a1a613a6e35cb148f876bf8c2f250e02076b2f58
Harsh <harshb15003@gmail.com>
2023-12-08 01:20:24 +0530

    dependencies

--- server/controller/mailer.js
+++ server/controller/mailer.js
@@ -1,7 +1,7 @@
 import nodemailer from 'nodemailer';
 import Mailgen from 'mailgen';
 
-import ENV from '../config.js';
+// import ENV from '../config.js';
 
 
 // https://ethereal.email/create
@@ -9,10 +9,10 @@ let nodeConfig = {
     host: "smtp.ethereal.email",
     port: 587,
     secure: false, // true for 465, false for other ports
-    auth: {
-        user: ENV.EMAIL, // generated ethereal user
-        pass: ENV.PASSWORD, // generated ethereal password
-    }
+    // auth: {
+    //     user: ENV.EMAIL, // generated ethereal user
+    //     pass: ENV.PASSWORD, // generated ethereal password
+    // }
 }
 
 let transporter = nodemailer.createTransport(nodeConfig);
--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -1,5 +1,5 @@
 import jwt from 'jsonwebtoken';
-import ENV from '../config.js'
+
 
 /** auth middleware */
 export default async function Auth(req, res, next){
@@ -8,7 +8,7 @@ export default async function Auth(req, res, next){
         // access authorize header to validate request
         const token = req.headers.authorization.split(" ")[1];
         // retrieve the user details for the logged in user
-        const decodedToken = await jwt.verify(token, ENV.JWT_SECRET);
+        const decodedToken = await jwt.verify(token, process.env.JWT_SECRET);
  
         req.user = decodedToken;
 
