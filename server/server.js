68a990e8ee73eb3fc4d390009167804f64127f1d
Harsh <harshb15003@gmail.com>
2023-12-07 23:58:09 +0530

    dependencies

--- server/server.js
+++ server/server.js
@@ -5,16 +5,27 @@ import connect from './database/conn.js';
 import router from './router/route.js';
 import bodyParser from 'body-parser';
 
-
 const app = express();
 
 /** middlewares */
 app.use(express.json());
 app.use(bodyParser.json());
-app.use(cors());
+
+// Configure CORS middleware
+const allowedOrigins = ['https://finance-future.netlify.app', ' https://event.api.drift.com/track']; // Add the origin of your React app
+app.use(cors({
+  origin: function (origin, callback) {
+    // Allow requests with no origin (like mobile apps or curl requests)
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+}));
+
 app.use(morgan('tiny'));
 app.disable('x-powered-by'); // less hackers know about our stack
-
 
 const port = 8080;
 
@@ -23,19 +34,20 @@ app.get('/', (req, res) => {
     res.status(201).json("Home GET Request");
 });
 
-
 /** api routes */
-app.use('/api', router)
+app.use('/api', router);
 
-/** start server only when we have valid connection */
-connect().then(() => {
+/** start server only when we have a valid connection */
+connect()
+  .then(() => {
     try {
-        app.listen(port, () => {
-            console.log(`Server connected to http://localhost:${port}`);
-        })
+      app.listen(port, () => {
+        console.log(`Server connected to http://localhost:${port}`);
+      });
     } catch (error) {
-        console.log('Cannot connect to the server')
+      console.log('Cannot connect to the server');
     }
-}).catch(error => {
-    console.log("Invalid database connection...!");
-})
+  })
+  .catch((error) => {
+    console.log('Invalid database connection...!');
+  });
